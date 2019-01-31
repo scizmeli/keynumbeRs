2b9311d994289d176c501a6f7003598d67e07284
Servet Ahmet Cizmeli <connect@pranageo.com>
2019-01-31 16:59:46 +0100

    Added time-series plotting code with ggplot2 and dygraphs

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -1,21 +1,45 @@
 library(keynumbers)
-a<-kn.getModel(modelname="sydney-water-usuage-1879")
+Sydney <-kn.getModel(modelname="sydney-water-usuage-1879")
 
-collnames <- kn.modelSegmentColNames(a)
+collnames <- kn.modelSegmentColNames(Sydney)
 
 coll2 <- kn.getCollection("3019")
 coll3 <- kn.getCollection("3023")
 coll2df2 <- kn.coll2df(coll2)
 coll2df3 <- kn.coll2df(coll3)
 
-kn.modelExec(a)
+kn.modelExec(Sydney)
 
 seg_num = 2
-Models <- kn.modelRep(a, seg_num)
+Models <- kn.modelRep(Sydney, seg_num)
 sapply(1:length(Models), function(x) kn.modelExec(Models[[x]]))
+
+
+if (! file.exists("AU.zip")) {
+  download.file("http://download.geonames.org/export/dump/AU.zip","AU.zip")
+  unzip("AU.zip")
+}
+AU <- read.delim("~/keynumbers/AU.txt", header=FALSE)
 
 seg_num = 3
-Models <- kn.modelRep(a, seg_num)
-sapply(1:length(Models), function(x) kn.modelExec(Models[[x]]))
+Models <- kn.modelRep(Sydney, seg_num)
+coll2df3[,"model1879"] <- sapply(1:length(Models), function(x) kn.modelExec(Models[[x]]))
 
+library(ggplot2)
+ggplot(data = coll2df3, aes(x = date, y = model1879))+
+  geom_line(color = "#00AFBB", size = 2)
 
+library(xts)
+library(dygraphs)
+library(dplyr)
+coll2df3 %>%
+  select(model1879) %>%
+  xts(coll2df3$date) %>%
+  dygraph() %>%
+  dySeries("model1879",strokeWidth = 4)
+
+library(leaflet)
+pal <- colorQuantile("YlOrRd", NULL, n = 8)
+leaflet(orstationc) %>%
+  addTiles() %>%
+  addCircleMarkers(color = ~pal(tann))
